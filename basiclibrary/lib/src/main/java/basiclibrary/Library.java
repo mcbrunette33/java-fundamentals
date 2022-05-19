9cad0986e6c153f3d8c2f7440044ef1a593722c6
mcbrunette33 <mcbrunette33@gmail.com>
2022-05-18 19:56:26 -0700

    added read me

--- basiclibrary/lib/src/main/java/basiclibrary/Library.java
+++ basiclibrary/lib/src/main/java/basiclibrary/Library.java
@@ -3,7 +3,11 @@
  */
 package basiclibrary;
 
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 
 public class Library {
 
@@ -33,17 +37,60 @@ public class Library {
         float sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
-            }
+        }
         return sum / array.length;
     }
-    public int[] calculateLowAvgArray(int[][] array){
+
+    public int[] calculateLowAvgArray(int[][] array) {
         int[] lowestAvgArray = array[0];
-        for(int i = 1; i < array.length; i++){
-          if(calculateAvgArray(lowestAvgArray) > calculateAvgArray(array[i])){
-              lowestAvgArray = array[i];
-          }
+        for (int i = 1; i < array.length; i++) {
+            if (calculateAvgArray(lowestAvgArray) > calculateAvgArray(array[i])) {
+                lowestAvgArray = array[i];
+            }
         }
         return lowestAvgArray;
     }
 
+    public String analyzingWeatherData(int[][] weather) {
+        int lowTemp = 100;
+        int highTemp = 0;
+        HashSet<Integer> uniqueTemps = new HashSet<>();
+        for (int[] array : weather) {
+            for (int value : array) {
+                uniqueTemps.add(value);
+                if (value < lowTemp) {
+                    lowTemp = value;
+                }
+                if (value > highTemp) {
+                    highTemp = value;
+                }
+            }
+        }
+        String response = "High:" + highTemp + "\n" + "Low:" + lowTemp + "\n";
+        for(int i = lowTemp; i < highTemp; i++){
+            if(!uniqueTemps.contains(i)){
+                response += "Never saw temperature:" + i + "\n";
+            }
+        }
+        return response;
+    }
+    public String tally(List<String> array){
+        HashMap<String, Integer> votes = new HashMap<>();
+        int numberOfVotes = 0;
+        String Winner = "";
+        for(String name : array){
+           if(!votes.containsKey(name)) {
+               votes.put(name, 0);
+           } else{
+               votes.put(name, votes.get(name) + 1);
+           }
+           for (String person : array){
+               if(votes.get(name) > numberOfVotes){
+                   numberOfVotes = votes.get(name);
+                   Winner = name;
+               }
+           }
+        }
+        return Winner + " got the most votes";
+    }
 }
